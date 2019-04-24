b278553e8b15b416b1d73d9ea590d408db72b204
shivamahuja <shivamahuja1140@gmail.com>
2019-04-25 02:57:30 +0530

    added POJOS. closes issue#2

---
+++ src/main/java/com/springframework/petclinic/model/Person.java
@@ -0,0 +1,22 @@
+package com.springframework.petclinic.model;
+
+public class Person {
+    private String firstName;
+    private String lastname;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
