8f3e17c655a350c826a405de592e588ed1dd839f
shivamahuja <shivamahuja1140@gmail.com>
2019-05-13 00:53:37 +0530

    automatic ID generation for map impl. Closes #23

--- pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/AbstractMapService.java
@@ -1,16 +1,15 @@
 package com.springframework.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.springframework.petclinic.model.BaseEntity;
+
+import java.util.*;
 
 /**
  * Created by shivam (techSavyThor) on 05/02/2019
  */
-public abstract class AbstractMapService<T,ID> {
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
 
-    protected Map<ID,T> map= new HashMap<>();
+    protected Map<Long,T> map= new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -20,8 +19,16 @@ public abstract class AbstractMapService<T,ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id,object);
+    T save(T object){
+        if(object!=null){
+            if(object.getId()==null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(),object);
+        }
+        else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
     void deleteById(ID id){
@@ -31,5 +38,13 @@ public abstract class AbstractMapService<T,ID> {
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
-
+    private Long getNextId(){
+        Long nextId;
+        try{
+            nextId= Collections.max(map.keySet())+1;}
+        catch(NoSuchElementException nse){
+            nextId=1L;
+        }
+        return nextId;
+    }
 }
--- pet-clinic-web/src/main/java/com/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/bootstrap/DataLoader.java
@@ -25,13 +25,11 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1=new Owner();
         owner1.setFirstName("Shivam");
         owner1.setLastname("Ahuja");
-        owner1.setId(1L);
         ownerService.save(owner1);
 
         Owner owner2 =new Owner();
         owner2.setFirstName("Thor");
         owner2.setLastname("TechSavy");
-        owner2.setId(2L);
         ownerService.save(owner2);
 
         System.out.println("Owner's loaded");
@@ -40,13 +38,11 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1=new Vet();
         vet1.setFirstName("Tony");
         vet1.setLastname("Stark");
-        vet1.setId(1L);
         vetService.save(vet1);
 
         Vet vet2=new Vet();
         vet2.setFirstName("Ant");
         vet2.setLastname("Man");
-        vet2.setId(2L);
         vetService.save(vet2);
 
         System.out.println("Vet's loaded");
