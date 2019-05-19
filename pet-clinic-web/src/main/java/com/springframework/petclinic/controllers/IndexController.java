9936c852fe3e3ee7b0bceec19d5fe9f7b1a12ad7
shivamahuja <shivamahuja1140@gmail.com>
2019-05-20 04:16:49 +0530

    added  broken links Closes #45

--- pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/IndexController.java
@@ -12,5 +12,10 @@ public class IndexController {
     public String index(){
         return "index";
     }
+    @RequestMapping({"/oups"})
+    public String oupsHandler(){
+
+        return "notimplemented";
+    }
 }
 
--- pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
@@ -22,4 +22,10 @@ public class OwnerController {
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping("/owners/find")
+    public String findOwners(){
+
+        return "notimplemented";
+    }
 }
