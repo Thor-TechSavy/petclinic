b6329cac2df34994d85fbd912297861beb8c2814
Shivam Ahuja <techsavythor@gmail.com>
2019-12-04 00:54:04 +0100

    updated owner, person, pet, pet type with JPA entity mapping. Closes #36

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Person.java
@@ -1,7 +1,14 @@
 package com.springframework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastname;
 
     public String getFirstName() {
