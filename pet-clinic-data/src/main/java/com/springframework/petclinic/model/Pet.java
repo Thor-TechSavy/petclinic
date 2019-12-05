73605cbbe74c1d21d57ae98010cb45c0fc00cb7d
Shivam Ahuja <techsavythor@gmail.com>
2019-12-06 00:06:17 +0100

    created Visit entity. closes #38

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Pet.java
@@ -1,7 +1,12 @@
 package com.springframework.petclinic.model;
 
+import org.hibernate.engine.internal.Cascade;
+
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
@@ -16,6 +21,9 @@ public class Pet extends BaseEntity {
     public void setName(String name) {
         this.name = name;
     }
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     @ManyToOne
     @JoinColumn(name = "type_id")
@@ -51,4 +59,8 @@ public class Pet extends BaseEntity {
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
+
+    public Set<Visit> getVisits() { return visits; }
+
+    public void setVisits(Set<Visit> visits) {this.visits = visits; }
 }
