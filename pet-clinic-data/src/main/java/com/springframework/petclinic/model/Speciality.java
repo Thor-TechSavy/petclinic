e12c86441dcb8e41b4a2138cbda516a38b692961
Shivam Ahuja <techsavythor@gmail.com>
2019-12-09 00:27:23 +0100

    added Project Lombok closes #53

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Speciality.java
@@ -1,5 +1,7 @@
 package com.springframework.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -8,14 +10,11 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "specialities")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
 public class Speciality extends BaseEntity{
     private String description;
-
-    public String getDescription() {
-        return description;
     }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-}
