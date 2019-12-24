b86db1d490e5aecd9c5ec694b3622397d682577f
Shivam Ahuja <techsavythor@gmail.com>
2019-12-24 19:12:33 +0100

    added jpa tests for owner services closes #50 #51

---
+++ pet-clinic-web/src/test/java/com/springframework/petclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,69 @@
+package com.springframework.petclinic.services.map;
+
+import com.springframework.petclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+    Long ownerId = 1L;
+    final String lastName = "Shivam";
+
+    @BeforeEach
+    void setUp() {
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+        ownerMapService.save(Owner.builder().id(1L).lastName(lastName).build());
+    }
+
+    @Test
+    void findAll() {
+
+        Set<Owner> ownerSet = ownerMapService.findAll();
+        assertEquals(1, ownerSet.size());
+    }
+
+    @Test
+    void deleteById() {
+        ownerMapService.deleteById(ownerId);
+    }
+
+    @Test
+    void delete() {
+        ownerMapService.delete(ownerMapService.findById(ownerId));
+        assertEquals(0, ownerMapService.findAll().size());
+    }
+
+    @Test
+    void saveExistingId() {
+        Long id = 2L;
+        Owner owner2 = Owner.builder().id(id).build();
+        Owner savedOwner = ownerMapService.save(owner2);
+        assertEquals(id, savedOwner.getId());
+    }
+
+    @Test
+    void saveNoId() {
+        Owner savedOwner = ownerMapService.save(Owner.builder().build());
+        System.out.println(savedOwner.getId());
+        assertNotNull(savedOwner);
+        assertNotNull(savedOwner.getId());
+    }
+
+    @Test
+    void findById() {
+        Owner owner = ownerMapService.findById(1L);
+        assertEquals(ownerId, owner.getId());
+    }
+
+    @Test
+    void findByLastName() {
+        Owner shivam = ownerMapService.findByLastName(lastName);
+        assertNotNull(shivam);
+        assertEquals(ownerId, shivam.getId());
+    }
+}
