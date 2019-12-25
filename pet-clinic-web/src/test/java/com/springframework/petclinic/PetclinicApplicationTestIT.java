1fa4e7dec5ea0aab4ef4c546b4fe37e4445387f9
Shivam Ahuja <techsavythor@gmail.com>
2019-12-25 17:06:53 +0100

    updated the pom

--- pet-clinic-web/src/test/java/com/springframework/petclinic/PetclinicApplicationTestIT.java
+++ pet-clinic-web/src/test/java/com/springframework/petclinic/PetclinicApplicationTestIT.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class PetclinicApplicationTests {
+public class PetclinicApplicationTestIT {
 
     @Test
     public void contextLoads() {
